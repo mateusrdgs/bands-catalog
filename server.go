5378477d54faa193e0c4b8fb86dbbe1967116043
Mateus Rodrigues <devrdgs@gmail.com>
2020-01-29 01:31:35 -0300

    feat: added but commented 'save_band' procedure call

--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 )
 
@@ -11,5 +13,22 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello world")
 	})
+
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/bands_catalog")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	// insert, err := db.Query("CALL save_band('Craft', 1998, 'Craft did not perform live until September 2014.', 'Sweden', 'Black Metal')")
+
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// defer insert.Close()
+
 	e.Logger.Fatal(e.Start(":1234"))
 }
