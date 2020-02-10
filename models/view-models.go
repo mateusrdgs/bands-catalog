4b968cafab034ce44f3071364a20fda3b436c7ef
Mateus Rodrigues <devrdgs@gmail.com>
2020-02-10 01:36:53 -0300

    feat: created 'Artist', 'Album' and 'Song' types

--- models/view-models.go
+++ models/view-models.go
@@ -1,10 +1,36 @@
 package models
 
-// Band struct serves as interface to bands
+// Band struct work as an interface to bands
 type Band struct {
-	Name             string `json:"band_name" form:"band_name"`
+	Name             string `json:"name" form:"name"`
 	YearOfFoundation int    `json:"year_of_foundation" form:"year_of_foundation"`
 	Biography        string `json:"biography" form:"biography"`
 	Country          string `json:"country" form:"country"`
 	Genre            string `json:"genre" form:"genre"`
+}
+
+// Artist struct work as an interface to artists
+type Artist struct {
+	Name       string `json:"name" form:"name"`
+	Age        int    `json:"age" form:"age"`
+	Country    string `json:"biography" form:"biography"`
+	Genre      string `json:"genre" form:"genre"`
+	Biography  string `json:"biography" form:"biography"`
+	Instrument string `json:"instrument" form:"instrument"`
+}
+
+// Album struct work as an interface to albums
+type Album struct {
+	Name        string `json:"name" form:"name"`
+	Type        string `json:"type" form:"type"`
+	ReleaseDate string `json:"release_date" form:"release_date"`
+	Label       string `json:"label" form:"label"`
+}
+
+// Song struct work as an interface to songs
+type Song struct {
+	Name     string `json:"name" form:"name"`
+	Number   int    `json:"number" form:"number"`
+	Duration int    `json:"duration" form:"duration"`
+	Lyrics   string `json:"lyrics" form:"lyrics"`
 }
